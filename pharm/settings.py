229c39a2e8fab76cc61efb73e7d7058a1b9faabb
hasibulislam999 <hasib143sl@gmail.com>
2022-06-19 13:29:40 +0600

    heroku push ready

--- pharm/settings.py
+++ pharm/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['pharmacymanage.herokuapp.com']
 ALLOWED_HOSTS = [
-    'pharmacy-management-system2.herokuapp.com',
+    'pms-pharmacy-management-system.herokuapp.com/',
     '127.0.0.1'
 ]
 
